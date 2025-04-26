c3492684c967b52e06269a48d616721632716f13
RezDead <razorjcgames@gmail.com>
2025-04-26 13:04:54 -0700

    Player Data Class

--- Source/Gauntlet/Public/PlayerData.h
+++ Source/Gauntlet/Public/PlayerData.h
@@ -6,6 +6,8 @@
 #include "Components/ActorComponent.h"
 #include "PlayerData.generated.h"
 
+//Declares an event
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnPlayerDeath);
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class GAUNTLET_API UPlayerData : public UActorComponent
@@ -15,14 +17,44 @@ class GAUNTLET_API UPlayerData : public UActorComponent
 public:	
 	// Sets default values for this component's properties
 	UPlayerData();
+	//UProperties
+
+	//Score
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = PlayerData)
+	int PlayerScore;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = PlayerData)
+	int HealthDecreaseRate;
+	
+	//Player Stats
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = PlayerStats)
+	int PlayerHealth;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = PlayerStats)
+	int PlayerSpeed;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = PlayerStats)
+	int PlayerArmor;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = PlayerStats)
+	int DamagePower;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = PlayerStats)
+	float AttackSpeed;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = PlayerStats)
+	float MagicPower;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = PlayerStats)
+	float ShotSpeed;
+
+	//Allows event to be called/processed in blueprint
+	UPROPERTY(BlueprintAssignable)
+	FOnPlayerDeath OnPlayerDeath;
+
+	//Functions
+	UFUNCTION(BlueprintCallable)
+	void TakeDamage(int Damage);
+
+	UFUNCTION(BlueprintCallable)
+	void IncreaseScore(int Score);
+
+	UFUNCTION()
+	void ReduceHealthEverySecond();
 
 protected:
-	// Called when the game starts
 	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
-
-		
 };
